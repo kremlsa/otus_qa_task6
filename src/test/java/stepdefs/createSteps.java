34a5ea39cf0eef8fbfde8fdcfdf066f32449b876
Aleksandr Kremlev <kremlsa@yandex.ru>
2021-03-16 22:26:07 +0700

    Initial commit

---
+++ src/test/java/stepdefs/createSteps.java
@@ -0,0 +1,27 @@
+package stepdefs;
+
+import io.cucumber.java.ru.Дано;
+import io.cucumber.java.ru.И;
+import io.cucumber.java.ru.Когда;
+import io.cucumber.java.ru.То;
+
+public class createSteps {
+
+
+    @Дано("Страница приложения")
+    public void loginPage() {
+
+    }
+
+    @И("данные пользователя")
+    public void данныеПользователя() {
+    }
+
+    @Когда("отправляется запрос на создание пользователя")
+    public void отправляетсяЗапросНаСозданиеПользователя() {
+    }
+
+    @То("возвращается ответ")
+    public void возвращаетсяОтвет() {
+    }
+}
