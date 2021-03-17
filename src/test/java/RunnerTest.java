e9e08eb71ba8c141bd4c83a3c20eb421b437a83e
sasha <kremlsa@yandex.ru>
2021-03-17 14:04:34 +0700

    make

--- src/test/java/RunnerTest.java
+++ src/test/java/RunnerTest.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features= "src/test/resources/features",
+@CucumberOptions(features= "src/test/resources",
         glue= {"stepdefs"},
         monochrome = true,
         plugin = {"pretty",
--- src/test/java/stepdefs/createSteps.java
+++ src/test/java/stepdefs/createSteps.java
@@ -4,24 +4,88 @@ import io.cucumber.java.ru.Дано;
 import io.cucumber.java.ru.И;
 import io.cucumber.java.ru.Когда;
 import io.cucumber.java.ru.То;
+import io.restassured.response.Response;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import service.UserService;
+import utils.Parser;
 
 public class createSteps {
 
+    UserService userService = new UserService();
+    JSONObject user;
+    Response response;
+    String userName;
+    JSONCompareMode compareMode;
 
-    @Дано("Страница приложения")
-    public void loginPage() {
-
+    @Дано("API на сайте {string}")
+    public void createContext(String url) {
+        userService.createContext(url);
     }
 
-    @И("данные пользователя")
-    public void данныеПользователя() {
+    @И("данные пользователя {string}")
+    public void createUser(String path) {
+        user = Parser.parseJSONFile("src/test/resources/" + path);
     }
 
     @Когда("отправляется запрос на создание пользователя")
-    public void отправляетсяЗапросНаСозданиеПользователя() {
+    public void sendRequest() {
+       response =  userService.addUserRequest(user.toString());
     }
 
-    @То("возвращается ответ")
-    public void возвращаетсяОтвет() {
+    @То("возвращается ответ со статусом {string}")
+    public void checkResponse(String responseCode) {
+        System.out.println(response.jsonPath().get("code"));
+        Assert.assertEquals(Integer.valueOf(responseCode), response.jsonPath().get("code"));
     }
+
+    @И("{string} пользователя равен указанному в файле {string}")
+    public void checkUserId(String attribute,  String filePath) {
+        Integer userId = null;
+        try {
+            userId = user.getInt(attribute);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Assert.assertEquals(userId.toString(), response.jsonPath().get("message"));
+    }
+
+    @И("имя пользователя через атрибут {string}")
+    public void setUserName(String attribute) {
+        try {
+            userName = user.getString(attribute);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        System.out.println(userName);
+    }
+
+    @Когда("отправляется запрос на получение сведений о пользователе")
+    public void getUserByName() {
+        response = userService.getUserRequest(userName);
+    }
+
+    @И("проверяем полное совпадение")
+    public void strictMode() {
+        compareMode = JSONCompareMode.STRICT;
+    }
+
+    @И("проверяем частичное совпадение")
+    public void lenientMode() {
+        compareMode = JSONCompareMode.LENIENT;
+    }
+
+    @То("данные в файле и ответе с сайта совпадают")
+    public void checkDataInResponse() {
+        String responseJsonString = response.body().asString();
+        try {
+            JSONAssert.assertEquals(user.toString(), responseJsonString, compareMode);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
