26a4586232099121a0bd6f2c5114610b29b84aaf
sasha <kremlsa@yandex.ru>
2021-04-12 09:20:30 +0700

    Устранение замечаний

--- src/test/java/RunnerTest.java
+++ src/test/java/RunnerTest.java
@@ -1,14 +1,8 @@
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-
-@RunWith(Cucumber.class)
 @CucumberOptions(features= "src/test/resources",
-        glue= {"stepdefs"},
-        monochrome = true,
-        plugin = {"pretty",
-                "html:target/cucumber-reports/task6.html"})
-public class RunnerTest {
+        glue= {"stepdefs"})
+public class RunnerTest extends AbstractTestNGCucumberTests {
 
 }
--- src/test/java/stepdefs/CreateSteps.java
+++ src/test/java/stepdefs/CreateSteps.java
@@ -4,16 +4,17 @@ import io.cucumber.java.ru.Дано;
 import io.cucumber.java.ru.И;
 import io.cucumber.java.ru.Когда;
 import io.cucumber.java.ru.То;
+import io.qameta.allure.Step;
 import io.restassured.response.Response;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.junit.Assert;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.testng.Assert;
 import service.UserService;
 import utils.Parser;
 
-public class createSteps {
+public class CreateSteps {
 
     UserService userService = new UserService();
     JSONObject user;
@@ -22,27 +23,31 @@ public class createSteps {
     JSONCompareMode compareMode;
     private final String PATH_TO_DATA = "src/test/testData/";
 
+    @Step("API на сайте \"{string}\"")
     @Дано("API на сайте {string}")
     public void createContext(String url) {
         userService.createContext(url);
     }
 
+    @Step(("данные пользователя \"{string}\""))
     @И("данные пользователя {string}")
     public void createUser(String path) {
         user = Parser.parseJSONFile(PATH_TO_DATA + path);
     }
 
+    @Step("отправляется запрос на создание пользователя")
     @Когда("отправляется запрос на создание пользователя")
     public void sendRequest() {
        response =  userService.addUserRequest(user.toString());
     }
 
+    @Step("возвращается ответ со статусом \"{string}\"")
     @То("возвращается ответ со статусом {string}")
     public void checkResponse(String responseCode) {
-        System.out.println(response.jsonPath().get("code"));
         Assert.assertEquals(Integer.valueOf(responseCode), response.jsonPath().get("code"));
     }
 
+    @Step("\"{string}\" пользователя равен указанному в файле \"{string}\"")
     @И("{string} пользователя равен указанному в файле {string}")
     public void checkUserId(String attribute,  String filePath) {
         Integer userId = null;
@@ -54,6 +59,7 @@ public class createSteps {
         Assert.assertEquals(userId.toString(), response.jsonPath().get("message"));
     }
 
+    @Step("имя пользователя через атрибут \"{string}\"")
     @И("имя пользователя через атрибут {string}")
     public void setUserName(String attribute) {
         try {
@@ -64,21 +70,25 @@ public class createSteps {
         System.out.println(userName);
     }
 
+    @Step("отправляется запрос на получение сведений о пользователе")
     @Когда("отправляется запрос на получение сведений о пользователе")
     public void getUserByName() {
         response = userService.getUserRequest(userName);
     }
 
+    @Step("проверяем полное совпадение")
     @И("проверяем полное совпадение")
     public void strictMode() {
         compareMode = JSONCompareMode.STRICT;
     }
 
+    @Step("проверяем частичное совпадение")
     @И("проверяем частичное совпадение")
     public void lenientMode() {
         compareMode = JSONCompareMode.LENIENT;
     }
 
+    @Step("данные в файле и ответе с сайта совпадают")
     @То("данные в файле и ответе с сайта совпадают")
     public void checkDataInResponse() {
         String responseJsonString = response.body().asString();
